2fbd3f5bd179023b01015533020f87122ffb7f89
geeklogbook <contact@geeklogbook.com>
2024-04-22 17:38:26 -0300

    virtual Envs

---
+++ code/spanish/coinbase/coinbase_request.py
@@ -0,0 +1,15 @@
+import requests
+
+def request_text_content_btc():
+    url = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
+    r = requests.get(url)
+    return r.text
+
+def request_text_content_eth():
+    url = "https://api.coinbase.com/v2/prices/ETH-USD/spot"
+    r = requests.get(url)
+    return r.text
+
+def coinbase_greeting():
+    return "This is coinbase_request.py"
+
---
+++ code/spanish/coinbase/main.py
@@ -0,0 +1,13 @@
+from coinbase_request import *
+from utils import *
+
+
+    
+if __name__ == "__main__":
+    test_virtual_env()
+    print("Main Program")
+    print(coinbase_greeting())
+    print(request_text_content_btc())
+    print(request_text_content_eth())
+
+
