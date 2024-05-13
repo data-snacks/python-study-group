c3f2f2dc09d30737d3d5b6f7146122283b989421
geeklogbook <contact@geeklogbook.com>
2024-05-13 17:47:54 -0300

    remove not used functions

--- code/spanish/infoleg/main.py
+++ code/spanish/infoleg/main.py
@@ -1,13 +1,8 @@
 from utils import *
 from infoleg_request import *
 
-import requests
-
 #Norma Ejemplo: https://servicios.infoleg.gob.ar/infolegInternet/anexos/35000-39999/37048/norma.htm
     
 if __name__ == "__main__":
     test_virtual_env()
-    print("Main Program")
-    #print(infoleg_greeting())
-    #print(request_print_context())
-    print(request_save_context())
+    print(request_infoleg())
