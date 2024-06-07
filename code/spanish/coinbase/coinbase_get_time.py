2fff7198c221c2bab4dcce71ad074b99dd07c08d
DataEngineeringTech <diegoeula.endava@gmail.com>
2024-06-07 14:27:03 -0300

    Class 8: Time, datetime and json

--- code/spanish/coinbase/coinbase_get_time.py
+++ code/spanish/coinbase/coinbase_get_time.py
@@ -1,3 +1,6 @@
+import requests
 # 'https://api.coinbase.com/v2/time'
 def get_time():
-    return "time"
+    url = "https://api.coinbase.com/v2/time"
+    r = requests.get(url)
+    return r.text
--- code/spanish/coinbase/main.py
+++ code/spanish/coinbase/main.py
@@ -1,6 +1,8 @@
 from coinbase_request import *
 from coinbase_get_time import *
 import time
+from datetime import datetime
+import json
     
 if __name__ == "__main__":
     cotizaciones = []
@@ -15,3 +17,22 @@ if __name__ == "__main__":
     print(cotizaciones, type(cotizaciones))
     set_cotizaciones = set(cotizaciones)
     print(set_cotizaciones, type(set_cotizaciones))
+    data_time=get_time()
+    print(data_time)
+    data_time_text = data_time.split('":"')[1]
+    print(data_time_text)
+    data_time_text = data_time_text.split('"')[0]
+    print(data_time_text)
+    datetime_object = datetime.strptime(data_time_text, '%Y-%m-%dT%H:%M:%SZ')
+    print(datetime_object)
+    print(datetime_object.year)
+    print(datetime_object.month)
+    print(datetime_object.day-1)
+    print(datetime_object.weekday())
+    tt = datetime_object.timetuple()
+    for it in tt:   
+        print(it)
+    data_time_json=json.loads(data_time)
+    print(data_time_json['data']['epoch'])
+
+    
